e767dacd95149e3ae845718ae602da63948c4cda
Mustafa Saifee <saifeemustafaq@gmail.com>
2015-11-08 18:46:24 +0530

    initial commmit

---
+++ retails.php
@@ -0,0 +1,157 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>Retails</title>
+<style>
+body{
+
+	top:0;
+	margin:0px;
+	padding:0px;
+	background:url('/images/3.jpg') no-repeat center center fixed;
+	
+}
+
+#upper{
+	
+	top:0;
+	width:100%;
+	height:100%;
+	border:1px solid white;
+
+}
+header{
+	top:0px;
+	width:100%;
+	margin:0px auto;
+
+
+}
+#hdiv{
+
+	width:1000px;
+	margin:0px auto;
+	border:1px solid white;
+}
+header h1{
+	
+	width:300px;
+	margin:0px auto;
+	font-size:50px;
+	text-align:center;
+	text-shadow:0px 0px 20px black;
+	color:white;
+}
+
+header form{
+	
+	width:1000px;
+	margin:0px auto;
+	margin-top:100px;
+	margin-bottom:30px;
+	//border:1px solid black;
+
+}
+
+header form input[type="text"]{
+	
+	width:1000px;
+	margin:10px auto;
+	padding:10px;
+	font-size:25px;
+	text-align:center;
+	//border-radius:16px;
+	box-shadow:inset 0px 0px 10px black;
+	outline:none;
+
+}
+#go{
+
+	width:300px;
+	margin:10px auto;
+	//border:1px solid black;
+
+
+}
+header form input[type="submit"]{
+	
+	
+	width:300px;
+	padding:5px;
+	margin:0px auto;
+	font-size:25px;
+	font-weight:bold;
+	color:white;
+	background-color:rgb(255,150,0);
+	outline:none;
+	
+
+}
+
+#search_cont{
+
+
+	width:300px;
+	height:300px;
+	margin:0px auto; 
+	border:1px solid black;
+	border-radius:100%;
+	
+
+
+}
+#btns{
+
+	width:1300px;
+	margin:0px auto;
+	margin-top:70px;
+	margin-bottom:70px;
+	//border:1px solid black;
+
+}
+.but{
+
+	width:200px;
+	height:50px;
+	padding:5px;
+	margin-top:100px;
+	margin-left:174px;
+	font-size:20px;
+	font-weight:bold;
+	color:white;
+	background-color:rgb(10,10,255);
+	border:none;
+	//border-radius:12px;
+	outline:none;
+
+}
+</style>
+</head>
+<body>
+
+<div id="upper">
+	<header>
+		<div id="hdiv"><img src="logos\logo.jpg" width="50px" height="50px" style="margin-left:297px; float:left;" ><h1>Aapli City <span style="font-size:35px; text-shadow:0px 0px 20px red;">Retails</span> </h1><div>
+		<form>
+			<input type="text" name="search" placeholder="Search">
+			<div id="go"><input type="submit" name="submitSearch" value="Go"></div>
+		</form>
+		<div id="btns">
+			<a href="#"><input type="button" class="but" value="Books,Stationary"></a>
+			<a href="#"><input type="button" class="but" value="Movies,Music"></a>
+			<a href="#"><input type="button" class="but" value="Mobiles And Tablet"></a>
+			<a href="#"><input type="button" class="but" value="Desktops"></a>
+			<a href="#"><input type="button" class="but" value="Home Appliances"></a>
+			<a href="#"><input type="button" class="but" value="Toys For Kids"></a>
+			<a href="#"><input type="button" class="but" value="Sports And Fitness"></a>
+			<a href="#"><input type="button" class="but" value="Beauty And Health"></a>
+			<a href="#"><input type="button" class="but" value="Clothing"></a>
+			<a href="#"><input type="button" class="but" value="Bags"></a>
+			<a href="#"><input type="button" class="but" value="Shoes"></a>
+			<a href="#"><input type="button" class="but" value="Medicals"></a>
+		
+		</div>
+	</header>
+</div>
+</body>
+</html>
---
+++ services.php
@@ -0,0 +1,160 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>Services</title>
+<style>
+body{
+
+	top:0;
+	margin:0px;
+	padding:0px;
+	background:url('/images/3.jpg') no-repeat center center fixed;
+	
+}
+
+#upper{
+	
+	top:0;
+	width:100%;
+	height:100%;
+	
+	border:1px solid white;
+
+}
+header{
+	top:0px;
+	width:100%;
+	margin:0px auto;
+
+
+}
+#hdiv{
+
+	width:1000px;
+	margin:0px auto;
+	border:1px solid white;
+}
+header h1{
+	
+	width:300px;
+	margin:0px auto;
+	font-size:50px;
+	text-align:center;
+	text-shadow:0px 0px 20px black;
+	color:white;
+}
+
+header form{
+	
+	width:1000px;
+	margin:0px auto;
+	margin-top:100px;
+	margin-bottom:30px;
+	//border:1px solid black;
+
+}
+
+header form input[type="text"]{
+	
+	width:1000px;
+	margin:10px auto;
+	padding:10px;
+	font-size:25px;
+	text-align:center;
+	//border-radius:16px;
+	box-shadow:inset 0px 0px 10px black;
+	outline:none;
+
+}
+#go{
+
+	width:300px;
+	margin:10px auto;
+	//border:1px solid black;
+
+
+}
+header form input[type="submit"]{
+	
+	
+	width:300px;
+	padding:5px;
+	margin:0px auto;
+	font-size:25px;
+	font-weight:bold;
+	color:white;
+	background-color:rgb(255,150,0);
+	outline:none;
+	
+
+}
+
+#search_cont{
+
+
+	width:300px;
+	height:300px;
+	margin:0px auto; 
+	border:1px solid black;
+	border-radius:100%;
+	
+
+
+}
+#btns{
+
+	width:1300px;
+	margin:0px auto;
+	margin-top:70px;
+	margin-bottom:70px;
+	//border:1px solid black;
+
+}
+.but{
+
+	width:200px;
+	height:50px;
+	padding:5px;
+	margin-top:100px;
+	margin-left:174px;
+	font-size:20px;
+	font-weight:bold;
+	color:white;
+	background-color:rgb(10,10,255);
+	border:none;
+	//border-radius:12px;
+	outline:none;
+
+}
+
+</style>
+</head>
+<body>
+
+<div id="upper">
+	<header>
+		<div id="hdiv"><img src="logos\logo.jpg" width="50px" height="50px" style="margin-left:297px; float:left;" ><h1>Aapli City <span style="font-size:35px; text-shadow:0px 0px 20px red;">Services</span> </h1></div>
+		<form>
+			<input type="text" name="search" placeholder="Search">
+			<div id="go"><input type="submit" name="submitSearch" value="Go"></div>
+		</form>
+		<div id="btns">
+			
+			<a href="#"><input type="button" class="but" value="Carpenters"></a>
+			<a href="#"><input type="button" class="but" value="Sweepers"></a>
+			<a href="#"><input type="button" class="but" value="Driver Service"></a>
+			<a href="#"><input type="button" class="but" value="Tailor"></a>
+			<a href="#"><input type="button" class="but" value="Electricians"></a>
+			<a href="#"><input type="button" class="but" value="Plumbers"></a>
+			<a href="#"><input type="button" class="but" value="Masons"></a>
+			<a href="#"><input type="button" class="but" value="Painters"></a>
+			<a href="#"><input type="button" class="but" value="Pest Control"></a>
+			<a href="#"><input type="button" class="but" value="House Repairs"></a>
+			<a href="#"><input type="button" class="but" value="Towing Service"></a>
+			<a href="#"><input type="button" class="but" value="Garage"></a>
+		
+		</div>
+	</header>
+</div>
+</body>
+</html>
